10209806ba0953bb2f13ec9815fcbc5cb2620ea3
Jon Guiles <jon.guiles@corporatereport.com>
2017-08-09 11:53:02 -0400

    Migrating code from bitbucket try 2.

--- _includes/topInc.php
+++
@@ -1,9 +0,0 @@
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">  
-<title></title>
-<link rel="shortcut icon" href="<?php echo $directory ?>" />
-<link rel="stylesheet" href="<?php echo $directory ?>_js/fancybox/jquery.fancybox.css" type="text/css" media="screen" />
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-<script type="text/javascript" src="<?php echo $directory ?>_js/fancybox/jquery.fancybox.pack.js"></script>
-<link rel="stylesheet" href="<?php echo $directory ?>_css/main.css"/>
-
